96139e957a546ece43dfb434d7d948ab5c19a615
alex <alex.musyoki@student.moringaschool.com>
2023-11-01 19:18:11 +0300

    quote

---
+++ script.js
@@ -0,0 +1,36 @@
+const quoteText = document.querySelector(".quote");
+authorName = document.querySelector(".author");
+quoteBtn = document.querySelector("button");
+soundBtn = document.querySelector(".sound");
+copyBtn = document.querySelector(".copy");
+shareBtn = document.querySelector(".twitter");
+heartBtn = document.querySelector(".like");
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(result => {
+        quoteText.innerText = result.content
+        authorName.innerText = result.author
+        quoteBtn.innerText = "New quote"
+        quoteBtn.classList.remove("loading")
+    });
+}
+
+soundBtn.addEventListener("click", () =>{
+    let utterance = new SpeechSynthesisUtterance( `${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+})
+
+copyBtn.addEventListener("click", () =>{
+    navigator.clipboard.writeText(quoteText.innerText);
+})
+
+shareBtn.addEventListener("click", () =>{
+    let twitter = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`
+    window.open(twitter, "_blank");
+})
+
+quoteBtn.addEventListener("click", randomQuote);
